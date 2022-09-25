204f4da157f9f7e7444acbc911ef6d22b9e9d8fe
Elena Trajkovska Samardzieva <elenatsamardzieva@gmail.com>
2022-09-25 15:38:59 +0200

    test

---
+++ NewTests.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class NewTests {
+
+    public NewTests() {
+    }
+
+    private  int a = 15;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
